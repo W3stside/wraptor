6287edec60ebd499306893719e2180a4040c02bb
David Sato <david.sato64@gmail.com>
2019-12-05 23:49:48 +0100

    removed test consts

--- src/const.ts
+++ src/const.ts
@@ -1,8 +1,2 @@
-// constants here
 export const ETH = 'ETH'
 export const TOKEN = 'TOKEN'
-// TEST DATA
-export const RINKEBY_WETH = '0xc778417e063141139fce010982780140aa0cd5ab'
-export const RINKEBY_GNO = '0xd0dab4e640d95e9e8a47545598c33e31bdb53c7c'
-export const INITIAL_INFURA_ENDPOINT = 'wss://rinkeby.infura.io/ws/v3/8b4d9b4306294d2e92e0775ff1075066'
-export const USER_ADDRESS = '0xfa3a5ba1864C4567aC77D50EcD91a2AaE92B650D'
