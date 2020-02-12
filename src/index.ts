1a608b6c2d0d6eb880644eb0428112c5fffdfded
David Sato <david.sato64@gmail.com>
2020-02-12 20:04:27 +0530

    forgot export, exported now

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export * from './types'
 export { default as useWraptor } from './hooks/useWraptor'
+export { default as useWeb3Setup } from './hooks/useWeb3Setup'
 export { default as WraptorComponent } from './components/WraptorComponent'
